ef28fb984281e6b3d0e0136f3a4be66b5b17a3c7
ergiev <maximergiev@gmail.com>
2016-04-27 17:59:05 +0300

    Домашнее задание 1-1 - Лесенка из звездочек

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//Показать на экране лесенку. Количество ступенек указывает пользователь.
+
+using System;
+
+namespace _1_stairs
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int step;
+
+            Console.WriteLine("Введите количество ступенек:");
+
+            try
+            {
+                UInt32 step = Convert.ToUInt32(Console.ReadLine());
+
+                if (step < 2)
+                {
+                    Console.WriteLine("Из этого количества ступеней лестницу не построишь!");
+                    Main();
+                }
+
+                Console.WriteLine("***");
+
+                for (int i = 0; i < step - 1; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                     Console.Write("  ");
+                    }
+                    Console.WriteLine("*");
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine("***");
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Это НЕ число!");
+                Main();
+            }
+
+            catch(OverflowException)
+            {
+                Console.WriteLine("Из этого количества ступеней лестницу не построишь!");
+                Main();
+            }
+
+        }
+    }
+}
